758ed16517bea89d53f09e963190383ec176b87b
Raza <razanaqvi813@gmail.com>
2023-10-15 16:38:28 +0500

    Change Database

--- api/main.py
+++ api/main.py
@@ -88,8 +88,8 @@ class News(db.Model):
         return f'<Book {self.title}>'
 
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 
 @app.route('/')
