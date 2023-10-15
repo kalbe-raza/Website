7fbb541adeeedc0ed3424460697c3afbb193a655
Raza <razanaqvi813@gmail.com>
2023-10-15 16:51:46 +0500

    Change Database

--- api/main.py
+++ api/main.py
@@ -68,7 +68,7 @@ app.jinja_env.filters['oneLine'] = oneLine
 app.jinja_env.filters['encode_spaces'] = encode_spaces
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:sVWjNPJ7l8Yd@ep-snowy-breeze-45989589-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:sVWjNPJ7l8Yd@ep-snowy-breeze-45989589.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("POSTGRES_MY-URL")
 
